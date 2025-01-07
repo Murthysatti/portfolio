08b5d417d676a073a042dc37ba9bcc0f8927cbe0
Murthy Satti <murthysatti321@gmail.com>
2025-01-07 18:23:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,33 @@ let homepage = document.getElementById("homepage");
 let educationpage = document.getElementById("educationpage");
 let contactpage = document.getElementById("contactpage");
 
+let navlist = document.getElementById("navlist");
+let sidebar= document.getElementById("sidebar");
+
 //nav bar events
 
 home.addEventListener("click",(e)=>{
     e.preventDefault();
-    homepage.scrollIntoView({behavior :"smooth"})
+    homepage.scrollIntoView({behavior :"smooth"});
+     navlist.style.display="none";
 });
 education.addEventListener("click",(e)=>{
     e.preventDefault();
-    educationpage.scrollIntoView({behavior :"smooth"})
+    educationpage.scrollIntoView({behavior :"smooth"});
+     navlist.style.display="none";
 });
 contact.addEventListener("click",(e)=>{
     e.preventDefault();
-    contactpage.scrollIntoView({behavior :"smooth"})
+    contactpage.scrollIntoView({behavior :"smooth"});
+     navlist.style.display="none";
 });
 
 //refresh 
 window.onload=function(){
     document.getElementById("homepage").scrollIntoView({behavior:"smooth"});
 }
+
 //media query
-let navlist = document.getElementById("navlist");
-let sidebar= document.getElementById("sidebar");
 
 sidebar.addEventListener("click",()=>{
     if (navlist.style.display==="none"){
